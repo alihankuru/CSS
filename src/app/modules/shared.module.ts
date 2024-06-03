fb75cf1c6a3d1306e418b5dd713925a93cd3f5d4
AK <ali.kuru6@hotmail.com>
2024-06-03 18:45:17 +0300

    -

--- src/app/modules/shared.module.ts
+++ src/app/modules/shared.module.ts
@@ -3,16 +3,17 @@ import { CommonModule } from '@angular/common';
 import { BlankComponent } from '../components/blank/blank.component';
 import { SectionComponent } from '../components/section/section.component';
 import { FormsModule } from '@angular/forms';
+import { FormValidateDirective } from 'form-validate-angular';
 
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule,BlankComponent, SectionComponent, FormsModule
+    CommonModule,BlankComponent, SectionComponent, FormsModule,FormValidateDirective
   ],
   exports:[
-    CommonModule,BlankComponent, SectionComponent, FormsModule
+    CommonModule,BlankComponent, SectionComponent, FormsModule,FormValidateDirective
   ]
 })
 export class SharedModule { }
